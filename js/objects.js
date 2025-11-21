23dd4609adf4f4dbca996413263ee7717c811886
Johnny <esp25goodlife@gmail.com>
2025-11-21 15:51:41 +0200

    1 function left

---
+++ js/objects.js
@@ -0,0 +1,54 @@
+let current_time = {
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+  print: function () {
+    console.log(`it's ${this.hours}:${this.minutes}:${this.seconds}`);
+  },
+  set_time: function () {
+    this.hours = +prompt(`enter the number of hours:`);
+    this.minutes = +prompt(`enter the number of minutes:`);
+    this.seconds = +prompt(`enter the number of seconds:`);
+  },
+  add_second: function () {
+    this.seconds += 1;
+    if (this.seconds == 60) {
+      this.minutes += 1;
+      this.seconds = 0;
+      if (this.minutes == 60) {
+        this.hours += 1;
+        this.minutes = 0;
+        if (hours == 24) {
+          this.hours = 0;
+          this.minutes = 0;
+          this.seconds = 0;
+        }
+      }
+    }
+  },
+};
+// current_time.print();
+// current_time.set_time();
+// current_time.print();
+// current_time.add_second();
+// current_time.print();
+// current_time.add_second();
+// current_time.print();
+// current_time.add_second();
+// current_time.print();
+// current_time.add_second();
+// current_time.print();
+
+//task 2
+let car = {
+  brand: 'Toyota',
+  model: 'Corola',
+  year: 2000,
+  average_speed: 80,
+  distance: 0,
+  time: 0,
+  duration_dest: function () {
+    this.distance = +prompt(`enter the distance to your destination`);
+    this.time = distance;
+  },
+};
