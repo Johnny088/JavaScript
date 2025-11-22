35cc741d07366f6c868abd64534c2b5c28db8e14
Johnny <esp25goodlife@gmail.com>
2025-11-22 12:45:49 +0200

    everything is done

--- js/objects.js
+++ js/objects.js
@@ -18,7 +18,7 @@ let current_time = {
       if (this.minutes == 60) {
         this.hours += 1;
         this.minutes = 0;
-        if (hours == 24) {
+        if (this.hours == 24) {
           this.hours = 0;
           this.minutes = 0;
           this.seconds = 0;
@@ -44,11 +44,15 @@ let car = {
   brand: 'Toyota',
   model: 'Corola',
   year: 2000,
-  average_speed: 80,
+  average_speed: 0,
   distance: 0,
   time: 0,
+  rest: 0,
   duration_dest: function () {
-    this.distance = +prompt(`enter the distance to your destination`);
-    this.time = distance;
+    this.distance = +prompt(`enter the distance to your destination:`);
+    this.average_speed = +prompt(`enter the average speed:`);
+    this.rest = Math.floor(this.distance / this.average_speed / 4);
+    this.time = this.distance / this.average_speed + this.rest;
+    console.log(`you need ${this.time} hours to reach your destination`);
   },
 };
