40e4daef024c6f717d9100a4776ba4fb75f8e1e6
Johnny <esp25goodlife@gmail.com>
2025-11-30 13:33:56 +0200

    done

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const getBtn = document.getElementById('get_info');
 const userName = document.getElementById('user');
 const regionName = document.getElementById('region');
 const followers = document.getElementById('followers');
+const following = document.getElementById('following');
 getBtn.onclick = () => {
   console.log(inputForm.value);
   showGitHubUserAvatar(inputForm.value);
@@ -14,10 +15,12 @@ getBtn.onclick = () => {
 // ------------------------------
 async function showGitHubUserAvatar(login) {
   console.log(`login: ${login}`);
-  userName.innerHTML = 'user name:\t' + login;
+  userName.innerHTML = login;
   const response = await fetch(gitHubUsers + '/' + login);
   const result = await response.json();
-  regionName.innerHTML = 'region:\t' + result.location;
+  regionName.innerHTML = result.location;
+  followers.innerHTML = result.followers;
+  following.innerHTML = result.following;
   console.log(result.avatar_url);
   avaImg.src = result.avatar_url;
 }
