8fa9b1af390fc56e44b872afb16bb8d353f7fe79
Johnny <esp25goodlife@gmail.com>
2025-12-02 13:38:30 +0200

    done

---
+++ main.js
@@ -0,0 +1,44 @@
+// https://dog.ceo/api/breeds/image/random Fetch!
+
+document.addEventListener('DOMContentLoaded', () => {
+  const image = document.getElementById('image');
+  image.hidden = true;
+});
+async function fetchCharacters() {
+  const url = 'https://dog.ceo/api/breeds/image/random';
+  const response = await fetch(url);
+  const data = await response.json();
+  image.hidden = false;
+  image.src = data.message;
+}
+
+// 	https://api.adviceslip.com/advice/search/{query}
+
+function fetchAdvice() {
+  const word = prompt(`type your word`).trim();
+  const url = 'https://api.adviceslip.com/advice/search';
+  fetch(`${url}/${word}`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.slips && data.slips.length > 0) {
+        advice.innerHTML = data.slips[0].advice;
+        console.log(data.slips[0].advice);
+      } else {
+        advice.innerHTML = "there isn't any advice with your word";
+      }
+    });
+}
+
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//         for (const char of data) {
+//             table.innerHTML += `<tr>
+//                         <th scope="row">${char.id}</th>
+//                         <td>${char.fullName}</td>
+//                         <td>${char.title}</td>
+//                         <td><img class="img-scale" height="50" src="${char.imageUrl}" alt="${char.fullName}"></td>
+//                     </tr>`;
+//         }
+//     })
+//     .catch(error => console.log(error));
